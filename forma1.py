1a7251044e6dfc9eb2ec1690f6745d54254404ee
Gerinova01 <kullai.gergo@diak.szbi-pg.hu>
2026-02-25 09:20:24 +0100

    dolgozat

--- forma1.py
+++ forma1.py
@@ -20,3 +20,113 @@ print("3. A legkevesebb futamot nyert versenyző: ____")
 print("4. A legtöbb futamot teljesített versenyző: ____")
 print("5. Az átlagos futamszám: ____")
 print("***A legtöbb futamgyőzelmet szerző csapat: ____")
+
+
+
+# Oh
+# Fernando Alonso
+# Oh
+# Fernando Alonso
+# Oh
+# Fernando Alonso
+# Oh
+# Fernando Alonso
+
+# Alonso
+# Alonso
+# Alonso
+# Alonso
+
+# Alonso
+# Alonso
+# Alonso
+
+# Oh
+# Fernando Alonso
+# Oh
+# Fernando Alonso
+# Oh
+# Fernando Alonso
+# Oh
+# Fernando Alonso
+
+# Alonso
+# Alonso
+# Alonso
+
+# Alonso
+# Alonso
+# Alonso
+
+# Oh
+# Fernando Alonso
+# Oh
+# Fernando Alonso
+# Oh
+# Fernando Alonso
+
+# Tu-tu-du-du
+# Max Verstappen
+# Tu-tu-du-du
+# Max Verstappen
+# Tu-tu-du-du
+# Max Verstappen
+# Tu-tu-du-du
+# Max Verstappn
+# Let's shake it to the Max
+# Put your hands up, hands up
+# Let's shake it to the Max
+# Put your hands up, hands up
+# Let's go!
+# Let's go!
+# Let's go!
+# Let's go!
+# Let's go! (go, go, go, go)
+# Max Verstappen
+
+# Tu-tu-du-du
+# Max Verstappen
+# Tu-tu-du-du
+# Max Verstappen
+# Tu-tu-du-du
+# Max Verstappen
+# Tu-tu-du-du
+# Max Verstappen
+
+# (That's how we do it)
+# (Simply, simply lovely)
+# (Yes! Woo!)
+# (Hahahaha)
+
+# (Yeah, here we go again)
+# (Oh my lord, Max!)
+# Yes! Yes!
+# Oh my God!
+# Woohoo!
+# Yes! Max Verstappen
+# You are the world champion
+# The world champion
+
+# Tu-tu-du-du
+# Max Verstappen
+# Tu-tu-du-du
+# Max Verstappen
+# Tu-tu-du-du
+# Max Verstappen
+# Tu-tu-du-du
+# Max Verstappen
+
+# The Start
+# Zero to one hundred in two seconds
+# Let’s go!
+
+# Schumacher
+
+# Schumacher
+
+# Dramatic in the box
+# Now must all go well
+# Schumacher twenty seconds before
+# Eighty liters refueled
+# Three, four, five and half seconds
+# And Schumac
--- konyvek.py
+++ konyvek.py
@@ -13,10 +13,62 @@ Olvasd be a konyvek.txt adatait, majd oldd meg az alábbi feladatokat!
 A megoldott feladatokat a kiirt_adatok nevű mappában hozd létre statisztika.txt néven!
 """
 
+konyvek = []
+with open('beolvasando_adatok/konyvek.txt', 'r', encoding='utf-8') as f:
+    next(f)
+    for sor in f:
+        adatok = sor.strip().split(';')
+        konyv = {'cím': adatok[0], 'szerző': adatok[1], 'oldalszám': int(adatok[2]), 'kiadó': adatok[3]}
+        konyvek.append(konyv)
 
-print("1. A beolvasott fájlban összesen ____ könyv szerepel.")
-print("2. A legtöbb oldalas könyv: ____")
-print("3. A legkevesebb oldalas könyv: ____")
-print("4. A legtöbb könyvet író szerző: ____")
-print("5. Az átlagos oldalszám: ____")
-print("***A legtöbb könyvet kiadó kiadó: ____")
+def legtobb(adat: str):
+    vizsgalando = konyvek[0]
+    for konyv in konyvek:
+        if konyv[adat] > vizsgalando[adat]:
+            vizsgalando = konyv
+    return vizsgalando["cím"]
+
+def legkevesebb(adat: str):
+    vizsgalando = konyvek[0]
+    for konyv in konyvek:
+        if konyv[adat] < vizsgalando[adat]:
+            vizsgalando = konyv
+    return vizsgalando["cím"]
+
+#1.feladat
+komyvek_szama = len(konyvek)
+
+#2.feladat
+leghosszabb_konyv = legtobb("oldalszám")
+
+#3.feladat
+legrovidebb_konyv = legkevesebb("oldalszám")
+
+#4.feladat
+legtobb_konyvet_iro = legtobb("szerző")
+
+#5.feladat
+osszes_oldal = 0
+for konyv in konyvek:
+    osszes_oldal += konyv["oldalszám"]
+    atlag_oldal = osszes_oldal/komyvek_szama
+
+#6.feladat
+legtobb_konyvet_kiado = legtobb("kiadó")
+
+
+print(f"1. A beolvasott fájlban összesen {komyvek_szama} könyv szerepel.")
+print(f"2. A legtöbb oldalas könyv: {leghosszabb_konyv}.")
+print(f"3. A legkevesebb oldalas könyv: {legrovidebb_konyv}.")
+print(f"4. A legtöbb könyvet író szerző: {legtobb_konyvet_iro}.")
+print(f"5. Az átlagos oldalszám: {atlag_oldal:.0f}. ")
+print(f"***A legtöbb könyvet kiadó kiadó: {legtobb_konyvet_kiado}.")
+
+
+with open('kiirt_adatok/statisztika.txt', 'w', encoding='utf-8') as c:
+    c.write(f"1. A beolvasott fájlban összesen {komyvek_szama} könyv szerepel.\n")
+    c.write(f"2. A legtöbb oldalas könyv: {leghosszabb_konyv}.\n")
+    c.write(f"3. A legkevesebb oldalas könyv: {legrovidebb_konyv}.\n")
+    c.write(f"4. A legtöbb könyvet író szerző: {legtobb_konyvet_iro}.\n")
+    c.write(f"5. Az átlagos oldalszám: {atlag_oldal:.0f}.\n")
+    c.write(f"***A legtöbb könyvet kiadó kiadó: {legtobb_konyvet_kiado}.\n")
